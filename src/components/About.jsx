f42f97e9f85ba10672def7fde7536d7cfd71b7b1
ilyas <elaissiilyas@gmail.com>
2023-08-16 00:21:45 +0100

    adding html necessary for about us

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,29 +1,53 @@
-import React from 'react'
+import { RiArrowRightSLine } from 'react-icons/ri'
 
 const About = () => {
   return (
     <div
       className='container mx-auto px-4 md:px-6 py-12'
     >
-      <div className=''>
-        <h3 className='text-light-dark-fg-primary md:pl-2 text-[40px] mb-8 leading-normal font-lexend not-italic font-medium'>Mobiling Social</h3>
+      <div>
+        <h3 className='text-light-dark-fg-primary md:pl-2 text-3xl sm:text-[40px] mb-8 leading-normal font-lexend not-italic font-medium capitalize'>mobiling social</h3>
+
         <div
           className='py-[30px] px-8 h-[400px] w-full rounded-[31px] bg-gradient'
         >
           <div
-            className='h-full w-full border border-white-fg-secondary border-dashed text-white-fg-secondary'
+            className='h-full w-full border border-white-fg-secondary border-dashed text-white-fg-secondary p-4 flex flex-col justify-end gap-5'
           >
-            <h4>
+            <h4 className='text-white text-2xl sm:text-3xl not-italic font-medium font-lexend'>
               Information on data processing
             </h4>
-            <p>
+            <p className='text-white text-sm sm:text-base not-italic font-normal font-lexend max-w-[665px]'>
               On our website we provide content from Storystream. To view this content, you must agree to the data processing by Storystream.
             </p>
-            <div>
-              <button>Agree</button>
-              <button>Information on data privacy</button>
+            <div className='flex gap-5'>
+              <button className='about-btn'>
+                <RiArrowRightSLine className='inline-block' />
+                Agree
+              </button>
+              <button className='about-btn'>
+                <RiArrowRightSLine className='inline-block' />
+                Information on data privacy
+              </button>
             </div>
           </div>
+        </div>
+      </div>
+
+      <div className='mt-12'>
+        <div>
+          <h3 className='text-light-dark-fg-primary md:pl-2 text-3xl sm:text-[40px] mb-2 leading-normal font-lexend not-italic font-medium capitalize'>
+            about us
+          </h3>
+          <p>
+            Find out briefly but informatively
+          </p>
+          <p>
+            As an upscale and exclusive chauffeur and limousine service near Indonesia, we have been ensuring for more than 15 years that you get to your destination on time and safely. We sell in around Indonesia , as well as beyond to destinations throughout World. From the Indonesia to shuttle service, you can look forward to a professional team and comfort of our car service.
+          </p>
+        </div>
+        <div>
+
         </div>
       </div>
 
